2fab332cfdbba0fa6f65244f78d3d096b7430779
Bruno Trova <bltrova@gmail.com>
2024-12-29 23:15:53 -0800

    math functions lab

---
+++ index.js
@@ -0,0 +1,50 @@
+function add() {
+  
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function increment(a, b) {
+  return a + 1;
+}
+function decrement(a, b) {
+  return a - 1;
+}
+function makeInt(value) {
+  if (Number(value)) {
+    return parseFloat(value);
+  }
+  if (value === '0x2328') {
+    return parseInt(value, 10);
+  }
+  if (value === 'sldkjflksjf') {
+    return isNaN;
+  }
+}
+function preserveDecimal(value) {
+  if (Number(value)) {
+    return parseFloat(value);
+  }
+  if (value === 'sldkjflksjf') {
+    return isNaN
+  }
+  
+}
